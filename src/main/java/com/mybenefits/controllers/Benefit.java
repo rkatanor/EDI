e8f6ff031976114418856004f5f9e9930e5ac969
rajkumar katanor <rkatanor@gmail.com>
2019-03-17 14:02:47 +0530

    content added

--- src/main/java/com/mybenefits/controllers/Benefit.java
+++ src/main/java/com/mybenefits/controllers/Benefit.java
@@ -25,7 +25,7 @@ public class Benefit {
 			}
 			@GetMapping(value="/hello")
 			public String hello() {
-				return "hello snapshot EDI data";
+				return "hello : EDI- sending data to employers";
 			}
 			public static void process() {
 				XmlMapper mapper = new XmlMapper();
